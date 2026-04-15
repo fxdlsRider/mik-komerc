5448e475948b77585113a4fa2e6ef8201ce4534d
fxdlsRider <zx501@icloud.com>
2026-04-15 16:34:28 +0200

    add burger menu to all pages

---
+++ js/burger-menu.js
@@ -0,0 +1,19 @@
+const burgerBtn = document.querySelector('.burger-menu-btn');
+const closeBtn = document.querySelector('.burger-close-btn');
+const burgerNav = document.querySelector('.burger-nav');
+const overlay = document.querySelector('.burger-overlay');
+
+burgerBtn.addEventListener('click', () => {
+    burgerNav.classList.add('open');
+    overlay.classList.add('open');
+});
+
+closeBtn.addEventListener('click', () => {
+    burgerNav.classList.remove('open');
+    overlay.classList.remove('open');
+});
+
+overlay.addEventListener('click', () => {
+    burgerNav.classList.remove('open');
+    overlay.classList.remove('open');
+});
